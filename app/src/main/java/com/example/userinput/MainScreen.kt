7bd08f7bb684db75cbb8694402666ff9074521de
Aldi Septiyanto <aldi.septiyanto.ft21@mail.umy.ac.id>
2024-10-30 18:20:46 +0700

    Fix Card Section

--- app/src/main/java/com/example/userinput/MainScreen.kt
+++ app/src/main/java/com/example/userinput/MainScreen.kt
@@ -94,6 +94,7 @@ fun MainScreen(
         Card (modifier.size(height = 200.dp, width = 300.dp)) {
             CardSection(judulParam = "Nama", isiParam = namaUser)
             CardSection(judulParam = "Jenis Kelamin", isiParam = selectedGenderUser)
+            CardSection(judulParam = "Email", isiParam = emailUser)
             CardSection(judulParam = "Alamat", isiParam = alamatUser)
             CardSection(judulParam = "NoHp", isiParam = noHPUser)
         }
